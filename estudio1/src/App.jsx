d0f603f53728ed93af8983a21d613a83a625bfac
Sebastián Tapia Rebolledo <sebastian@MacBook-Air-de-Sebastian.local>
2024-03-11 13:34:52 -0300

    firs commit

---
+++ estudio1/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+export function App() {
+    const API_URL_FACT = "https://catfact.ninja/fact"
+    const API_URL_IMAGE = 'https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red'
+
+    const [fact, setFact] = useState()
+
+    useEffect(() => {
+        fetch(API_URL_FACT)
+            .then(res => res.json())
+            .then(data => {
+                const {fact} = data
+                setFact(fact)
+            })
+    }, [])
+
+    const firstWord = fact.split(' ')[0]
+
+    return (
+        <main>
+            <h1>Hola Mundo</h1>
+            {fact && <p>{fact}</p>}
+            <br />
+            <h2>{firstWord}</h2>
+        </main>
+    );
+}
