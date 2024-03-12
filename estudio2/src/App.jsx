339a9fc6de5f0fc28ebcbb6b8be911226c98785f
Sebastián Tapia Rebolledo <sebastian@MacBook-Air-de-Sebastian.local>
2024-03-12 13:22:53 -0300

    arreglé el imgUrl que estaba mal declarado

--- estudio2/src/App.jsx
+++ estudio2/src/App.jsx
@@ -4,22 +4,34 @@ export function App() {
 
     const API_URL_FACT = "https://catfact.ninja/fact"
     const API_URL_IMAGE = 'https://cataas.com/cat/says/${firstWord}?fontSize=50&fontColor=red'
+    const API_URL = 'https://cataas.com/'
 
-    const [frase, setFrase] = useState();
+    const [fact, setFact] = useState();
+    const [imageUrl, setImageUrl] = useState();
 
     useEffect(() => {
-        .then(data =>{
-            setFrase(fact);
-            const threeFristWord = fact.split(' ',3);
-            fetch(API_URL)
-        })
-            
-    }, []);
+        fetch(API_URL_FACT)
+            .then(res => res.json())
+            .then(data => {
+                const { fact } = data;
+                setFact(fact);
+
+                const threeFirstWords = fact.split(' ', 3)
+                console.log(threeFirstWords)
+
+                fetch('https://cataas.com/cat/says/${threeFirstWords}?size=50&color=red&json=true')
+                    .then(res => res.json())
+                    .then(response => {
+                        const { url } = response
+                        setImageUrl('https://cataas.com/${url}')
+                    })
+            })
+    }, [])
 
     return (
         <main>
             <h1>Hola mundo</h1>
-            <p>{frase}</p>
-            <p>{frase.split(" ", 4)}</p>
+            <p>{fact}</p>
+            {imageUrl && <img src={imageUrl} alt={'Imagen de: ${fact}'} />}
         </main>);
 }
