a8fdbf15de6e304d76f20fd80153fa60f1ba4cff
Sebastián Tapia Rebolledo <sebastian@MacBook-Air-de-Sebastian.local>
2024-03-12 18:22:45 -0300

    commit mac

---
+++ ecommerce-youtube/src/components/Producto.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <img src="{{urlImg}}">
+        <h3>{{nombre}}</h3>
+        <p>{{precio}}</p>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Producto',// nombre del componente, tiene que coincidir con el nombre del archivo
+    components: {},//
+    methods: {},
+    props:{ // estos son las variables que van a ingresar desde el componente padre.
+        nombre: String,
+        precio: Number,
+        urlImg: String,
+    }
+}
+</script>
+<style scoped></style>
