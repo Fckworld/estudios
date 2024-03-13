ee23bd36d13ff6f76105feaeb30281240fc9bcaa
Fckworld <sebatapiareb@gmail.com>
2024-03-12 23:25:38 -0300

    windows

---
+++ ecommerce-vite-vue/src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="flex p-8 justify-center">
+        <input type="text" class="rounded border-2 border-gray-200 w-full" placeholder="Search for meals" />
+        <div class="flex gap-1">
+            <router-link to="/" v-for="letter of letters">
+                {{ letter }}
+            </router-link>
+        </div>
+    </div>
+</template>
+<script setup>
+
+
+import { computed } from 'vue';
+import store from '../store';
+
+const meals = computed(() => store.state.meals)
+const letters = 'ABCDEFGHJIKLMNOPQRSTUVWXYZ'.split('')
+
+
+</script>
